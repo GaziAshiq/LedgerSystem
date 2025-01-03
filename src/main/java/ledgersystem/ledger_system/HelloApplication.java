054c8f374a8141b5fa264a20a321af5b62ee0ef0
Gazi Ashiq <GaziAshiq.Dev@gmail.com>
2025-01-03 20:17:51 +0600

    show a Launching msg

--- src/main/java/ledgersystem/ledger_system/HelloApplication.java
+++ src/main/java/ledgersystem/ledger_system/HelloApplication.java
@@ -19,6 +19,7 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         System.out.println("Hello, JavaFX!");
+        System.out.println("Launching JavaFX application...");
         launch();
     }
 }
